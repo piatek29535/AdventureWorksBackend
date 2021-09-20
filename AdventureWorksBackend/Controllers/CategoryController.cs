738ab675bee5f9849ce615e1c95b5310e9eb614e
piatek29535 <adamex12343@gmail.com>
2021-09-20 18:14:50 +0200

    just a basic init, connect to a database to finish

---
+++ AdventureWorksBackend/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using AdventureWorksBackend.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdventureWorksBackend.Controllers
+{
+    [ApiController]
+    [Route("categories")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly CategoriesContext _context;
+        public CategoryController(CategoriesContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IEnumerable<Categories> Get()
+        {
+            return _context.Categories.ToList();
+        }
+
+        
+
+        [Route("error")]
+        public string Error()
+        {
+            return "Error";
+        }
+    }
+}
---
+++ AdventureWorksBackend/Model/CategoriesContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdventureWorksBackend.Model
+{
+    public class CategoriesContext : DbContext
+    {
+        public CategoriesContext(DbContextOptions<CategoriesContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Categories> Categories { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+    }
+}
